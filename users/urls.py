7b04b2d84b7d83d82f882c2deba0a2485386e336
Valenciavm <141790853+Valenciavm@users.noreply.github.com>
2025-10-24 18:23:37 -0500

    He implementado todo lo necesario para crear el componente propio para la app usuarios, he movido templates de login, registro, about, y perfil, puse las vistas correspondientes y modifiqué urls para el correcto funcionamiento de la app

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile' ),
+    path('about/', views.about, name='about'),
+]
---
+++ users/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+def about(request):
+    return render(request, 'about.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Cuenta creada para {username}!')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Usuario o contraseña incorrectos')
+    return render(request, 'login.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')
+
+def profile(request):
+    profile = request.user
+    return render(request, 'profile.html', {'profile': profile})
+
